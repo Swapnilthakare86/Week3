7a15eee56d8b72a0f90e5522d456a1082a661596
Swapnilthakare86 <sthakare@xtsworld.in>
2026-02-24 16:55:57 +0530

    Add backend project

--- Employee_Management/middleware/validation.middleware.js
+++
@@ -1,8 +0,0 @@
-exports.validateEmployee = (req, res, next) => {
-  const { employee_code, first_name, email } = req.body;
-
-  if (!employee_code || !first_name || !email) {
-    return res.status(400).json({ error: "employee_code, first_name, email required" });
-  }
-  next();
-};
